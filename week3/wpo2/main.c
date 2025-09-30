d766ab502512ec8e1d8ff3e4e9eff92837e799d9
Kobe Vervoort <39643347+vervoortkobe@users.noreply.github.com>
2025-09-30 11:32:33 +0200

    add print_christmas_tree

--- week3/wpo2/main.c
+++ week3/wpo2/main.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include "fizzbuzz.h"
-#include "calc_cosine.h"
 #include "print_christmas_tree.h"
 
 int main(void) {
     fizzbuzz(20);
-    calc_cosine(3.1416, 7);
-    print_christmas_tree(5);
+    //calc_cosine(3.1416, 7);
+    print_christmas_tree(7);
 
     return 0;
 }
--- week3/wpo2/print_christmas_tree.h
+++ week3/wpo2/print_christmas_tree.h
@@ -1,4 +1,23 @@
 // https://dodona.be/en/courses/5451/series/63960/activities/544298707
 #include <stdio.h>
 
+void print_christmas_tree(int n) {
+    for (int i = 1; i <= n; i++) {
+        if (i == 1) printf("*");
+        else {
+            for (int j = 1; j <= i+1; j++) {
+                printf("*");
+            }
+        }
+        printf("\n");
+    }
 
+    for (int i = 1; i <= (n-1)/2; i++) {
+        printf(" ");
+    }
+    printf("*\n");
+    for (int i = 1; i <= (n-1)/2; i++) {
+        printf(" ");
+    }
+    printf("*\n");
+}
