114b8aece456544a8f7811fd371593a41fd161ae
Kobe Vervoort <39643347+vervoortkobe@users.noreply.github.com>
2025-09-30 16:49:48 +0200

    solve insertion_sort but error!

--- week3/wpo2/insertion_sort.h
+++ week3/wpo2/insertion_sort.h
@@ -1,1 +1,21 @@
 // https://dodona.be/en/courses/5451/series/63960/activities/726260036
+
+void insertion_sort(char string[]) {
+    char gesorteerd[strlen(string) + 1];
+    gesorteerd[strlen(string)] = '\0';
+    gesorteerd[0] = string[0];
+
+    for (int i = 1; i < strlen(string); i++) {
+        char key = string[i];
+        int j = i - 1;
+
+        while (j >= 0 && key < string[j]) {
+            string[j + 1] = string[j];
+            j -= 1;
+        }
+        string[j + 1] = key;
+        gesorteerd[j + 1] = key;
+    }
+
+    printf(gesorteerd);
+}
--- week3/wpo2/main.c
+++ week3/wpo2/main.c
@@ -5,6 +5,7 @@
 //#include "calc_cosine.h"
 #include "print_perfect_numbers.h"
 #include "convert_hex_to_dec.h"
+#include "insertion_sort.h"
 
 int main(void) {
     //izzbuzz(20);
@@ -12,7 +13,8 @@ int main(void) {
     //print_christmas_tree(7);
     //print_primes(100);
     //print_perfect_numbers(100);
-    convert_hex_to_dec("5F");
+    //convert_hex_to_dec("5F");
+    insertion_sort("randomstring");
 
     return 0;
 }
