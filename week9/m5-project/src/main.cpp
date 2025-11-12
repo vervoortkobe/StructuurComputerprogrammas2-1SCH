1b5480cdb084d2b0a9db2a08c3a85b6a7018de0a
Kobe Vervoort <39643347+vervoortkobe@users.noreply.github.com>
2025-11-12 10:51:10 +0100

    EEPROM

--- week9/m5-project/src/main.cpp
+++ week9/m5-project/src/main.cpp
@@ -13,47 +13,41 @@ void setup() {
   M5.Lcd.fillScreen(BLACK);
 }
 
-#define RECT_WIDTH 50
-#define RECT_HEIGHT 50
+#define MEM_SIZE 512
+uint8_t x = 0;
+uint8_t y = 0;
 
-float x = -1;
-float y = -1;
-float z = 0;
+  void save_Position() {
+    int address  = 0;
+    EEPROM.writeByte(address, x);
+    address++;
+    EEPROM.writeByte(address, y);
+    EEPROM.commit();
+  }
+  void read_Position() {
+    int address  = 0;
+    x = EEPROM.readByte(address);
+    address++;
+    y = EEPROM.readByte(address);
+  }
 
 void loop() {
+  M5.Lcd.fillScreen(BLACK);
   M5.update();
-
-  float ax = 0, ay = 0, az = 0;
-  M5.Imu.getAccelData(&ax, &ay, &az);
-
-  if (x < 0) {
-    x = (M5.Lcd.width() - RECT_WIDTH) / 2;
-    y = (M5.Lcd.height() - RECT_HEIGHT) / 2;
-    M5.Lcd.fillRect((int)x, (int)y, RECT_WIDTH, RECT_HEIGHT, RED);
+  if (M5.BtnA.wasPressed()) {
+    x++;
+    save_Position();
   }
-
-  M5.Lcd.fillRect((int)x, (int)y, RECT_WIDTH, RECT_HEIGHT, BLACK);
-
-  const float threshold = 0.06f;
-  const float scale = 15.0f;
-
-  if (fabsf(ax) > threshold) x += ax * scale; // left/right tilt
-  if (fabsf(ay) > threshold) y += ay * scale; // forward/back tilt
-
-  if (x < 0) x = 0;
-  if (y < 0) y = 0;
-  if (x > M5.Lcd.width() - RECT_WIDTH) x = M5.Lcd.width() - RECT_WIDTH;
-  if (y > M5.Lcd.height() - RECT_HEIGHT) y = M5.Lcd.height() - RECT_HEIGHT;
-
-  M5.Lcd.fillRect((int)x, (int)y, RECT_WIDTH, RECT_HEIGHT, RED);
-
-  float deltaX = (fabsf(ax) > threshold) ? ax * scale : 0.0f;
-  if (deltaX != 0.0f) {
-    M5.Lcd.fillRect((int)x, (int)y, RECT_WIDTH, RECT_HEIGHT, BLACK);
-    x -= 2.0f * deltaX;
-    if (x < 0) x = 0;
-    if (x > M5.Lcd.width() - RECT_WIDTH) x = M5.Lcd.width() - RECT_WIDTH;
-    M5.Lcd.fillRect((int)x, (int)y, RECT_WIDTH, RECT_HEIGHT, RED);
+  if (M5.BtnB.wasPressed()) {
+    y++;
+    save_Position();
   }
+  if (M5.BtnC.wasPressed()) {
+    x = 0;
+    y = 0;
+    save_Position();
+  }
+  read_Position();
+  M5.Lcd.fillRect(x, y, 160, 80, RED);
   delay(20);
 }
