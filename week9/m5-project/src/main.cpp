2c60dff5496c3d68b5ce0042b77d4fe8cab90a0c
Kobe Vervoort <39643347+vervoortkobe@users.noreply.github.com>
2025-11-12 10:31:40 +0100

    smooth move using accel

--- week9/m5-project/src/main.cpp
+++ week9/m5-project/src/main.cpp
@@ -4,10 +4,7 @@
 #include <stdlib.h>
 #include "EEPROM.h"
 
-// The setup routine runs once when M5StickC starts up
 void setup() {
-  
-  // Initialize the M5StickC object
   M5.begin();
   M5.Imu.init();
   Serial.begin(115200);
@@ -19,28 +16,44 @@ void setup() {
 #define RECT_WIDTH 50
 #define RECT_HEIGHT 50
 
-// The loop routine keeps looping as long as the controller is on
-uint8_t x = 0;
-uint8_t y = 0;
-void loop(){
+float x = -1;
+float y = -1;
+float z = 0;
+
+void loop() {
   M5.update();
+
+  float ax = 0, ay = 0, az = 0;
+  M5.Imu.getAccelData(&ax, &ay, &az);
+
+  if (x < 0) {
+    x = (M5.Lcd.width() - RECT_WIDTH) / 2;
+    y = (M5.Lcd.height() - RECT_HEIGHT) / 2;
+    M5.Lcd.fillRect((int)x, (int)y, RECT_WIDTH, RECT_HEIGHT, RED);
+  }
+
+  M5.Lcd.fillRect((int)x, (int)y, RECT_WIDTH, RECT_HEIGHT, BLACK);
+
+  const float threshold = 0.06f;
+  const float scale = 15.0f;
+
+  if (fabsf(ax) > threshold) x += ax * scale; // left/right tilt
+  if (fabsf(ay) > threshold) y += ay * scale; // forward/back tilt
+
+  if (x < 0) x = 0;
+  if (y < 0) y = 0;
+  if (x > M5.Lcd.width() - RECT_WIDTH) x = M5.Lcd.width() - RECT_WIDTH;
+  if (y > M5.Lcd.height() - RECT_HEIGHT) y = M5.Lcd.height() - RECT_HEIGHT;
+
+  M5.Lcd.fillRect((int)x, (int)y, RECT_WIDTH, RECT_HEIGHT, RED);
+
+  float deltaX = (fabsf(ax) > threshold) ? ax * scale : 0.0f;
+  if (deltaX != 0.0f) {
+    M5.Lcd.fillRect((int)x, (int)y, RECT_WIDTH, RECT_HEIGHT, BLACK); // erase current
+    x -= 2.0f * deltaX; // undo and invert
+    if (x < 0) x = 0;
+    if (x > M5.Lcd.width() - RECT_WIDTH) x = M5.Lcd.width() - RECT_WIDTH;
+    M5.Lcd.fillRect((int)x, (int)y, RECT_WIDTH, RECT_HEIGHT, RED); // redraw at corrected pos
+  }
   delay(20);
-  if(M5.BtnA.isPressed() && M5.BtnB.isPressed()) {
-    M5.Lcd.fillRect(x, y, RECT_WIDTH, RECT_HEIGHT, BLACK);
-    x = 0;
-    y = 0;
-  } else if(M5.BtnA.isPressed()) {
-    M5.Lcd.fillRect(x, y, RECT_WIDTH, RECT_HEIGHT, BLACK);
-    x += 10;
-  } else if(M5.BtnB.isPressed()) {
-    M5.Lcd.fillRect(x, y, RECT_WIDTH, RECT_HEIGHT, BLACK);
-    y += 10;
-  }
-  if(x > M5.Lcd.width()) {
-    x = 0;
-  }
-  if(y > M5.Lcd.height()) {
-    y = 0;
-  }
-  M5.Lcd.fillRect(x, y, RECT_WIDTH, RECT_HEIGHT, RED);
 }
